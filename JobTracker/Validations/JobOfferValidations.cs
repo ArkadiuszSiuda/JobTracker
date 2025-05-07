0e99579ae5aa77e454ef1991f9a322f35f4df6ff
Arkadiusz.Siuda1@gmail.com <Arkadiusz.Siuda1@gmail.com>
2025-05-07 23:10:16 +0200

    fixed Update method, started implementation of validations

---
+++ JobTracker/Validations/JobOfferValidations.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using JobTracker.Entities;
+
+namespace JobTracker.Validations;
+
+public class JobOfferValidations : AbstractValidator<JobOffer>
+{
+    public JobOfferValidations()
+    {
+        RuleFor(x => x.CompanyName)
+             .NotEmpty()
+             .WithMessage("Company name is required.");
+        RuleFor(x => x.Position)
+             .NotEmpty()
+             .WithMessage("Position is required.");
+        RuleFor(x => x.Status)
+             .NotEmpty()
+             .WithMessage("Status is required.");
+        RuleFor(x => x.SalaryRange)
+             .Null()
+             .WithMessage("Salary range is not supported yet.");
+        RuleFor(x => x.Note)
+             .Length(0, 100)
+             .WithMessage("Note must be less than 500 characters.");
+        RuleFor(x => x.Link)
+             .NotEmpty()
+             .WithMessage("Link is required.")
+             .Must(link => Uri.IsWellFormedUriString(link, UriKind.Absolute))
+             .WithMessage("Link must be a valid URL.");
+        RuleFor(x => x.SubmissionDate)
+             .Must(date => date != default(DateTime));
+        RuleFor(x => x.Status)
+             .Null()
+             .WithMessage("Status is not supported yet.");
+
+    }
+}
